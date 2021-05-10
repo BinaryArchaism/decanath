a2edd078ae41db9d111090b4320ea1a3d9ff0217
dmitriidmitrievk@gmail.com <dmitriidmitrievk@gmail.com>
2021-05-10 22:14:36 +0300

    getStatements backend

--- internal/api/setMark.go
+++ internal/api/setMark.go
@@ -1,20 +1,22 @@
 package api
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 func SetMark(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	studentName := string(body)
-	for _, w := range body {
-		fmt.Print(string(w))
-	}
-	fmt.Println(studentName)
+	//body, err := ioutil.ReadAll(r.Body)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//var studentName string
+	//var subjectId uint
+	//var value uint
+	//for _, w := range string(body) {
+	//	if (string(w) != ' ') {
+	//		studentName += string(w)
+	//	}
+	//	fmt.Print(string(w))
+	//}
+
 }
